8efaf9dd69b664cfdadc39cae14267167d816252
Leonardo MP <ridelant02@gmail.com>
2026-09-21 12:01:07 -0600

    refactor: mejorar manejo de errores

--- backend/src/core/utils/errors.js
+++ backend/src/core/utils/errors.js
@@ -1,12 +1,13 @@
 
 
 class AppError extends Error {
-    constructor(message, statusCode = 500) {
+    constructor(message, status = 500) {
         super(message);
-        this.statusCode = statusCode;
-
+        this.status = status;
+        this.isOperational = true;
         // Mantiene correctamente el nombre de la clase
         this.name = this.constructor.name;
+        Error.captureStackTrace(this, this.constructor);
     }
 }
 
@@ -22,8 +23,36 @@ class BadRequestError extends AppError {
     }
 }
 
-export {
+class UnauthorizedError extends AppError {
+    constructor(message = 'No autenticado') {
+        super(message, 401);
+    }
+}
+
+class ForbiddenError extends AppError {
+    constructor(message = 'No tienes permisos para realizar esta acción') {
+        super(message, 403);
+    }
+}
+
+class ConflictError extends AppError {
+    constructor(message = 'El recurso ya existe o entra en conflicto') {
+        super(message, 409);
+    }
+}
+
+class ValidationError extends AppError {
+    constructor(message = 'Los datos proporcionados no son válidos', errors=null) {
+        super(message, 422);
+        this.errors = errors;
+    }
+}
+module.exports = {
     AppError,
     NotFoundError,
-    BadRequestError
-};
+    BadRequestError,
+    UnauthorizedError,
+    ForbiddenError,
+    ConflictError,
+    ValidationError,
+};   
