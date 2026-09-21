948402bccc79d0a8ed3ad033eb28ccc6c5ba0aaf
Leonardo MP <ridelant02@gmail.com>
2026-09-21 12:03:43 -0600

    refactor: integrar rutas de pacientes

--- backend/src/opticas/index.js
+++ backend/src/opticas/index.js
@@ -4,10 +4,16 @@ const router = express.Router();
 const usuariosRoutes = require('./usuarios/routes/usuarios.routes');
 const sucursalesRoutes = require('./sucursales/routes/sucursales.routes');
 
+const pacienteRoutes = require('./pacientes/routes/paciente.routes');
+
 // /api/optica/usuarios/...
 router.use('/usuarios', usuariosRoutes);
 
 // /api/optica/sucursales/...
 router.use('/sucursales', sucursalesRoutes);
+
+
+// /api/optica/sucursales/  Seccion pacientes -- V.1.0
+router.use('/pacientes', pacienteRoutes);
 
 module.exports = router;
